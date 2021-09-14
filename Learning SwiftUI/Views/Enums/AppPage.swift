4fea4227a93bb90076ecc9a1445562b29f191d09
Layth Yousif <leo.yousif@intimetec.com>
2021-09-14 00:26:02 -0600

    Test generated documentation

--- Learning SwiftUI/Views/Enums/AppPage.swift
+++ Learning SwiftUI/Views/Enums/AppPage.swift
@@ -6,6 +6,18 @@
 //
 import SwiftUI
 
+/**
+ Another description
+ 
+ More description
+ 
+ - important: Make sure you read this
+ - returns: a Llama spotter rating between 0 and 1 as a Float
+ - parameter by: The number of Llamas spotted on the trip
+ - version: 1.0
+ 
+ Even more description
+ */
 enum AppPage {
     case home
     case page1
